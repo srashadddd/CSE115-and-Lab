47bb06cb72894029d2189523b89f4ce924ec366b
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2025-06-12 22:52:29 +0600

    Create ch01 miletokm sample.c

---
+++ ch01 miletokm sample.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define KMS_PER_MILE 1.609
+
+int main(void) {
+    float miles, kms;
+    printf("Enter the distance in miles> ");
+    scanf("%f", &miles);
+    kms = KMS_PER_MILE * miles;
+    printf("That equals %.2f kilometers.\n", kms);
+    return 0;
+}
