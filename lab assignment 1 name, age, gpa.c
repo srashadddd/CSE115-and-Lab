96a12dac2825e263b175a836aab7c0c981db8cdc
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2025-06-12 22:38:35 +0600

    Add files via upload

---
+++ lab assignment 1 name, age, gpa.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char firstLetter;
+    int age;
+    float gpa;
+
+    printf("Enter the first letter of your name: ");
+    scanf(" %c", &firstLetter);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your expected GPA for this course: ");
+    scanf("%f", &gpa);
+
+    printf("Your name's first letter: %c\n", firstLetter);
+    printf("Your age: %d\n", age);
+    printf("Your expected GPA: %.2f\n", gpa);
+
+    return 0;
+}
