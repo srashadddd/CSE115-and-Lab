d39cbc29cb6470732b3d52990a9e64e71f72e8b5
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2025-06-17 23:16:05 +0600

    Update ch01 miletokm sample.c

--- ch01 miletokm sample.c
+++ ch01 miletokm sample.c
@@ -2,7 +2,9 @@
 #define KMS_PER_MILE 1.609
 
 int main(void) {
+    
     float miles, kms;
+    
     printf("Enter the distance in miles> ");
     scanf("%f", &miles);
     kms = KMS_PER_MILE * miles;
