39253834d32c1f90749bcaedf63e8d0794532a8f
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2025-07-02 17:46:26 +0600

    Update ch03 shape drawing.c

--- ch03 shape drawing.c
+++ ch03 shape drawing.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-// Draws a circle (head of stick figure)
+
 void draw_circle(void) {
     printf("   *   \n");
     printf(" *   * \n");
     printf("   *   \n");
 }
 
-// Draws a triangle (could be used as lower part or decoration)
 void draw_triangle(void) {
     printf("    ^   \n");
     printf("   / \\\n");
@@ -16,11 +15,10 @@ void draw_triangle(void) {
     printf("/-------\\\n");
 }
 
-// Main function that calls all drawing functions to form a stick figure
 int main(void) {
     printf("\nNow drawing a circle:\n");  
-    draw_circle();      // Head
+    draw_circle();    
     printf("\nNow drawing a triangle:\n");
-    draw_triangle();    // Optional triangle example
+    draw_triangle();   
     return 0;
 }
