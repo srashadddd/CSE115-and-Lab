622a1246076919b4ba4fd1710050ec651eac1b93
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2025-07-02 17:32:48 +0600

    Create lab quiz 1 set A no1.c

---
+++ lab quiz 1 set A no1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int secondLargest(int a, int b, int c) {
+    if ((a > b && a < c) || (a > c && a < b))
+        return a;
+    else if ((b > a && b < c) || (b > c && b < a))
+        return b;
+    else
+        return c;
+}
+
+int main() {
+    int num1, num2, num3;
+    printf("Enter three numbers:\n");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    int second = secondLargest(num1, num2, num3);
+    printf("Second largest number is: %d\n", second);
+    return 0;
+}
