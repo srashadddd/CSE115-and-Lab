8b7ca47f33b13c9fa26bdfac60ce2c0e071f063d
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2025-07-02 17:37:17 +0600

    Update lab quiz 1 set A no1.c

--- lab quiz 1 set A no1.c
+++ lab quiz 1 set A no1.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 
-int secondLargest(int a, int b, int c) {
-    if ((a > b && a < c) || (a > c && a < b))
-        return a;
-    else if ((b > a && b < c) || (b > c && b < a))
-        return b;
-    else
-        return c;
-}
+int secondLargest(int, int, int);
 
 int main() {
     int num1, num2, num3;
@@ -16,4 +9,13 @@ int main() {
     int second = secondLargest(num1, num2, num3);
     printf("Second largest number is: %d\n", second);
     return 0;
+}
+
+int secondLargest(int a, int b, int c) {
+    if ((a > b && a < c) || (a > c && a < b))
+        return a;
+    else if ((b > a && b < c) || (b > c && b < a))
+        return b;
+    else
+        return c;
 }
