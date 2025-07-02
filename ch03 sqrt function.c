a92479ed18981643254ee0ed896868cafffb6752
Murashahun-Al-Masud Srashad <srashadddd17@gmail.com>
2025-07-02 17:46:47 +0600

    Update ch03 sqrt function.c

--- ch03 sqrt function.c
+++ ch03 sqrt function.c
@@ -3,24 +3,21 @@
 
 int main(void)
 {
-    double first, second;         // Input - two data values
-    double first_sqrt;            // Output - square root of first
-    double second_sqrt;           // Output - square root of second
-    double sum_sqrt;              // Output - square root of sum
+    double first, second;     
+    double first_sqrt;          
+    double second_sqrt;        
+    double sum_sqrt;             
 
-    // Get first number and display its square root
     printf("Enter the first number: ");
     scanf("%lf", &first);
     first_sqrt = sqrt(first);
     printf("The square root of the first number is %.2f\n", first_sqrt);
 
-    // Get second number and display its square root
     printf("Enter the second number: ");
     scanf("%lf", &second);
     second_sqrt = sqrt(second);
     printf("The square root of the second number is %.2f\n", second_sqrt);
 
-    // Display the square root of the sum of the two numbers
     sum_sqrt = sqrt(first + second);
     printf("The square root of the sum of the two numbers is %.2f\n", sum_sqrt);
 
